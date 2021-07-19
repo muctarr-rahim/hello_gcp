4a1f9ddbbb8381a6e8b008032fc58fcb15f0735b
Muctarr Rahim <muctarr.hassani@gmail.com>
2021-07-19 17:38:32 +0000

    fix typo

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
-import logging
 
-from flask import Flask, app
+from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
-    """Returan a friendly HTTP greeting."""
-    return 'Hello World!'
+    """Return a friendly HTTP greeting."""
+    return 'Hello I like to make AI Apps'
 
-app.route('/newroute/<name>')
-def newroute(name):
-    """Parameter"""
-    return "This was passed in: %s" %name
-
-
-app.error_handler(500)
-def server_error(e):
-    logging.exception('An error occured during the request.')
-    return"""
-    An internal error occured: <pre>{}</pre>
-    see logs for full stackrace.
-    """.format(e), 500
-
+@app.route('/name/<value>')
+def name(value):
+    val = {"value": value}
+    return jsonify(val)
 
 if __name__ == '__main__':
-    #This is used when running locally. Gunicorn is used to run the 
-    # application on Google App Engine. See entry point in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
